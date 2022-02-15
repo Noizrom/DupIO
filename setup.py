4579def2d67ed982784dae811cb7605cfb3e893a
Noizrom <61880430+Noizrom@users.noreply.github.com>
2022-02-15 15:18:54 +0800

    fixed entry point for pip install

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="DupIO",
     version="0.1.0",
     py_modules=["DuplicateIO"],
+    author="Noizrom",
+    author_email="noizrom@gmail.com",
     entry_points='''
         [console_scripts]
-        DuplicateIO=DuplicateIO'''
+        DuplicateIO=DuplicateIO:__main__''',
+    url = "https://github.com/Noizrom/DupIO",
+    download_url = "https://github.com/Noizrom/DupIO/releases/tag/new"
 )
