9ed3211d17a718db5b8af819b2d9342830bacbdc
Noizrom <61880430+Noizrom@users.noreply.github.com>
2022-03-06 21:21:38 +0800

    Update duplicate_io.py
    Pylint suggestion fixes

--- duplicate_io.py
+++ duplicate_io.py
@@ -1,41 +1,59 @@
+'''
+duplicates stdout or stderr to a file
+'''
+
+
 import sys
 
-class DupIO(object):
+class DupIO():
     """duplicate an io to file"""
+    _io = None
     def __init__(self ,name , mode:str = "a+" , header:str = None):
-        self.file = open(name , mode)
+        self.file = open(name , mode , encoding='utf-8')
         self.header = header
+
+
     def write(self ,data):
-        self.io.write(data)
+        '''generally write into file'''
+        self._io.write(data)
         if self.header:
             data = self.header + " : " + data
         self.file.write(data)
 
+
     def flush(self):
+        '''i dunno. flush'''
         self.file.flush()
 
-class duplicatestdout(DupIO):
+
+class Forkstdout(DupIO):
     """duplicate an stdout to file"""
     def __init__(self , name , mode:str = "a+" , header:str =None):
-        self.io = sys.stdout    
+        self._io = sys.stdout
         super().__init__(name , mode , header)
         sys.stdout = self
+
+
     def __del__(self):
-        sys.stdout = self.io
+        sys.stdout = self._io
         self.file.close()
 
-class duplicatestderr(DupIO):
+
+class Forkstderr(DupIO):
     """duplicate an stderr to file"""
     def __init__(self , name , mode:str = "a+" , header:str =None):
-        self.io = sys.stderr   
+        self.io = sys.stderr
         super().__init__(name , mode , header)
         sys.stderr= self
+        
+
     def __del__(self):
         sys.stderr= self.io
         self.file.close()
 
+
 if __name__=="__main__":
-    # test Duplicated output
-    import time
-    duplicatestdout("rdout.txt" , "a+")
+    Forkstdout("rdout.txt" , "a+")
     print('Hello World')
+    
+    
